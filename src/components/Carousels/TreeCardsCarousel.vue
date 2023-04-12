eca0ea63a7dd4129518458ba496d7f1560c580c8
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-04-12 11:25:23 -0600

    Add background

--- src/components/Carousels/TreeCardsCarousel.vue
+++ src/components/Carousels/TreeCardsCarousel.vue
@@ -1,42 +1,45 @@
 <template>
-  <div class="flex justify-center bg-primary">
-    <swiper
-      :slidesPerView="1"
-      :spaceBetween="50"
-      :navigation="true"
-      :breakpoints="{
-        '640': {
-          slidesPerView: 2,
-          spaceBetween: 120
-        },
-        '1024': {
-          slidesPerView: 2,
-          spaceBetween: 140
-        },
-        '1536': {
-          slidesPerView: 3,
-          spaceBetween: 135
-        },
-        '1920': {
-          slidesPerView: 3,
-          spaceBetween: 200
-        },
-        '2560': {
-          slidesPerView: 3,
-          spaceBetween: 300
-        },
-        '3840': {
-          slidesPerView: 3,
-          spaceBetween: 260
-        }
-      }"
-      :modules="modules"
-      class="my-20 flex justify-center px-8 md:max-w-screen-md md:px-14 lg:max-w-screen-lg lg:px-32 2xl:max-w-screen-2xl 3xl:max-w-screen-3xl 3xl:px-48 4xl:my-28 4xl:max-w-screen-4xl 5xl:max-w-screen-4xl 5xl:px-20"
-    >
-      <swiper-slide v-for="(text, index) in content" :key="index" class="">
-        <TreeCarouselCard :cardContent="text" />
-      </swiper-slide>
-    </swiper>
+  <div class="relative">
+    <div class="flex justify-center bg-primary md:basis-3/4">
+      <swiper
+        :slidesPerView="1"
+        :spaceBetween="50"
+        :navigation="true"
+        :breakpoints="{
+          '640': {
+            slidesPerView: 2,
+            spaceBetween: 120
+          },
+          '1024': {
+            slidesPerView: 2,
+            spaceBetween: 140
+          },
+          '1536': {
+            slidesPerView: 3,
+            spaceBetween: 135
+          },
+          '1920': {
+            slidesPerView: 3,
+            spaceBetween: 200
+          },
+          '2560': {
+            slidesPerView: 3,
+            spaceBetween: 300
+          },
+          '3840': {
+            slidesPerView: 3,
+            spaceBetween: 260
+          }
+        }"
+        :modules="modules"
+        class="mb-20 flex justify-center px-8 md:my-20 md:max-w-screen-md md:px-14 lg:max-w-screen-lg lg:px-32 2xl:max-w-screen-2xl 3xl:max-w-screen-3xl 3xl:px-48 4xl:my-28 4xl:max-w-screen-4xl 5xl:max-w-screen-4xl 5xl:px-20"
+      >
+        <swiper-slide v-for="(text, index) in content" :key="index" class="">
+          <TreeCarouselCard :cardContent="text" />
+        </swiper-slide>
+      </swiper>
+    </div>
+    <div class="absolute right-0 top-0 h-full w-full bg-[#fff] md:w-1/4"></div>
   </div>
 </template>
 
@@ -83,5 +86,11 @@ export default {
 .swiper-button-next,
 .swiper-button-prev {
   color: #000;
+}
+
+@media (min-width: 768px) {
+  .swiper-button-prev {
+    color: #fff;
+  }
 }
 </style>
