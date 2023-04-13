e6d4efa0080b0754d63f83462558256dcd2bdb5e
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-04-13 00:04:27 -0600

    Finish About Us Page

---
+++ src/components/About/ImageLeft.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mx-4 mb-10 md:mx-8 md:mb-20 lg:mx-0 lg:flex lg:gap-10 4xl:mb-32">
+    <div class="mb-6 lg:mb-0 lg:basis-5/12">
+      <img :src="content.image" alt="Image" class="h-full w-full" />
+    </div>
+
+    <div class="lg:flex lg:basis-7/12 lg:flex-col lg:justify-center lg:py-20 3xl:py-28">
+      <div
+        v-for="paragraph in content.paragraphs"
+        class="mb-6 lg:mb-10 lg:w-5/6 lg:last-of-type:mb-0 3xl:mb-16"
+      >
+        <h2
+          class="mb-3 text-2xl font-semibold text-primary md:text-3xl lg:text-4xl 2xl:text-5xl 4xl:text-6xl 5xl:text-7xl"
+        >
+          {{ paragraph.title }}
+        </h2>
+
+        <p class="text-lg 2xl:text-xl 3xl:text-2xl 4xl:mb-12 4xl:text-3xl 5xl:text-4xl">
+          {{ paragraph.desc }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageLeft',
+  props: {
+    content: Object
+  }
+}
+</script>
--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -6,6 +6,8 @@
   <Team :content="teamContent" />
 
   <Stars :content="cardStarContent" />
+
+  <ImageLeft :content="imageLeftContent" />
 </template>
 
 <script>
@@ -13,6 +15,7 @@ import SecondHero from '@/components/General/SecondHero.vue'
 import ImageRight from '@/components/About/ImageRight.vue'
 import Team from '@/components/General/Team.vue'
 import Stars from '@/components/General/Stars.vue'
+import ImageLeft from '@/components/About/ImageLeft.vue'
 
 export default {
   name: 'AboutUsView',
@@ -51,14 +54,28 @@ export default {
           title: 'Lorem impsum',
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
         }
-      ]
+      ],
+      imageLeftContent: {
+        image: 'src/assets/img/DESTINOS/CARTAGENA/CARTAGENA_3.webp',
+        paragraphs: [
+          {
+            title: 'Lorem Ipsum',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum, sem ac sollicitudin consequat, dui velit placerat ex, et tincidunt ipsum nibh nec risus.'
+          },
+          {
+            title: 'Lorem Ipsum',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum, sem ac sollicitudin consequat, dui velit placerat ex, et tincidunt ipsum nibh nec risus.'
+          }
+        ]
+      }
     }
   },
   components: {
     SecondHero,
     ImageRight,
     Team,
-    Stars
+    Stars,
+    ImageLeft
   }
 }
 </script>
